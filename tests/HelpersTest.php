0b619bcb699f0438f20261932a4c5a7b30b1109a
ItsMurumba <kevmurumba@gmail.com>
2022-10-26 16:33:10 -0400

    Add a new Test for helpers

---
+++ tests/HelpersTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Itsmurumba\Mpesa\Test;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class HelpersTest extends TestCase
+{
+    protected $mpesa;
+
+    public function setUp(): void
+    {
+        $this->mpesa = Mockery::mock('Itsmurumba\Mpesa\Mpesa');
+        $this->mock = Mockery::mock('GuzzleHttp\Client');
+    }
+
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    /**
+     * Test that helper returns correct
+     *
+     * @return void
+     */
+    function it_returns_instance_of_mpesa()
+    {
+        $this->assertInstanceOf("Itsmurumba\Mpesa\Mpesa", $this->mpesa);
+    }
+}
