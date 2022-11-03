59ece36be5b5611e5762c8f8d12c156de0b87f0a
ItsMurumba <kevmurumba@gmail.com>
2022-11-03 15:45:05 -0400

    Instal Mpesa configuratio file

---
+++ src/Console/InstallMpesaPackage.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Itsmurumba\Mpesa\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class InstallMpesaPackage extends Command
+{
+    protected $signature = 'mpesa:install';
+
+    protected $description = 'Install Mpesa Laravel Package';
+
+    public function handle()
+    {
+        $this->info('Installing Laravel Mpesa......');
+        $this->info('Publishing mpesa configuration');
+
+        if (!$this->configExists('mpesa.php')) {
+            $this->publishConfiguration();
+            $this->info('Publishing mpesa configuration');
+        } else {
+            if ($this->shouldOverwriteConfig()) {
+                $this->info('Overwitting mpesa configuration file......');
+                $this->publishConfiguration($force = true);
+            } else {
+                $this->info('Existing mpesa configuration was not overwritten');
+            }
+        }
+
+        $this->info('Installed Mpesa Package');
+    }
+
+    private function configExists($fileName)
+    {
+        return File::exists(config_path($fileName));
+    }
+
+    private function shouldOverwriteConfig()
+    {
+        return $this->confirm('Config file alaready exists. Do you want to overwrite it?', false);
+    }
+
+    private function publishConfiguration($forcePublish = false)
+    {
+        $params = [
+            '--provider' => "Itsmurumba\Mpesa\MpesaServiceProvider",
+            '--tag' => "config"
+        ];
+
+        if ($forcePublish === true) {
+            $params['--force'] = true;
+        }
+
+        $this->call('vendor:publish', $params);
+    }
+}
