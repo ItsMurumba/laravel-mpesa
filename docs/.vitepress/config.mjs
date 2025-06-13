9bb195d2f4a6b9c78dd6eb942910fba7bc460140
ItsMurumba <kevmurumba@gmail.com>
2025-06-13 07:57:51 -1100

    feat: adds sidebar menu items

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -29,6 +29,45 @@ export default defineConfig({
                     {text: 'Progress', link: '/introduction/progress'},
                 ]
             },
+            {
+                text: 'Security',
+                collapsed: true,
+                items: [
+                    {text: 'Authorization', link: '/security/authorization'},
+                ]
+            },
+            {
+                text: 'Payments',
+                collapsed: true,
+                items: [
+                    {text: 'B2B Express Checkout', link: '/payments/b2b-express-checkout'},
+                    {text: 'B2C Account Top Up', link: '/payments/b2c-account-top-up'},
+                    {text: 'Business Pay Bill', link: '/payments/paybill'},
+                    {text: 'Business Buy Goods', link: '/payments/buy-goods'},
+                    {text: 'Customer to Business', link: '/payments/c2b'},
+                    {text: 'Dynamic QR', link: '/payments/dynamic-qr'},
+                    {text: 'Mpesa Express', link: '/payments/mpesa-express'},
+                    {text: 'Mpesa Ratiba', link: '/payments/mpesa-ratiba'},
+                    {text: 'Tax Remittance', link: '/payments/tax-remittance'},
+                ]
+            },
+            {
+                text: 'Disbursements',
+                collapsed: true,
+                items: [
+                    {text: 'Business to Customer', link: '/disbursements/b2c'},
+                ]
+            },
+            {
+                text: 'Experience',
+                collapsed: true,
+                items: [
+                    {text: 'Account Balance', link: '/experience/account-balance'},
+                    {text: 'Bill Manager', link: '/experience/bill-manager'},
+                    {text: 'Transaction Status', link: '/experience/transaction-status'},
+                    {text: 'Reversals', link: '/experience/reversals'},
+                ]
+            },
         ],
 
         socialLinks: [
